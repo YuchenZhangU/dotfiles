5c758fda3f615c8305cf7f23f96f07645ad094c9
Yuchen Zhang <yuz899@utulsa.edu>
2018-01-27 13:07:53 -0500

    improve install by prompt message

--- install.sh
+++ install.sh
@@ -1,13 +1,17 @@
+#!/bin/bash
 
 echo "source ~/dotfiles/.bashrc" >> ~/.bashrc
+echo ".bashrc installed ..."
 
 echo "[include]" >> ~/.gitconfig
 echo "   path = ~/dotfiles/.gitconfig" >> ~/.gitconfig
+echo ".gitconfig installed ..."
 
 echo "source ~/dotfiles/.vimrc" >> ~/.vimrc
+echo ".vimrc installed ..."
 
 echo "source ~/dotfiles/.tmux.conf" >> ~/.tmux.conf
-
+echo ".tmux.conf installed ..."
 
 if [ ! -d ~/.ssh ]; then
     cp -r .ssh ~/.ssh
